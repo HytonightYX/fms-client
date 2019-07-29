8fe6501dbc9b847cfa0f40ba71c4378eeb8b490d
HytonightYX <whosiyuan@qq.com>
2019-07-29 21:34:00 +0800

    feat: 添加档案模态框

--- src/pages/singleFIle/index.js
+++ src/pages/singleFIle/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Button, Table } from 'antd'
+import { Card, Button, Table, Modal, Form, Select, Input, Icon } from 'antd'
 import { single_file_columns, single_file_data } from '../../config/table.config'
 import FilterForm from '../../components/Form/FilterForm'
 
@@ -8,6 +8,7 @@ class SingleFile extends Component {
 	state = {
 		selectedRowKeys: [],
 		loading: false,
+		showAddFile: false
 	};
 
 	start = () => {
@@ -26,10 +27,25 @@ class SingleFile extends Component {
 		this.setState({ selectedRowKeys });
 	};
 
+	/**
+	 * 添加档案弹框
+	 */
+	handleAddFile = () => {
+		this.setState({
+			showAddFile: true
+		})
+	}
 
+	/**
+	 * 提交添加档案请求
+	 */
+	submitAddFile = () => {
+		// TODO 提交请求
+		const fileData = console.log(this.addFileFormData.props.form.getFieldsValue())
+		alert('提交成功')
+	}
 
 	render() {
-
 		const { loading, selectedRowKeys } = this.state;
 		const rowSelection = {
 			selectedRowKeys,
@@ -46,7 +62,7 @@ class SingleFile extends Component {
 
 				<div className="table-warp">
 					<Card className='table-op-card'>
-						<Button type={'primary'}>添加档案</Button>
+						<Button type={'primary'} onClick={this.handleAddFile}>添加档案</Button>
 						<Button >修改档案</Button>
 						<Button type={'danger'}>删除档案</Button>
 					</Card>
@@ -59,9 +75,63 @@ class SingleFile extends Component {
 						rowSelection={rowSelection}
 					/>
 				</div>
+
+				<Modal
+					title='添加档案'
+					visible={this.state.showAddFile}
+					onCancel={() => {
+						this.setState({showAddFile: false})
+					}}
+					onOk={this.submitAddFile}
+				>
+					<AddSingleFileModalForm wrappedComponentRef={(data) => {this.addFileFormData = data}}/>
+				</Modal>
 			</div>
 		)
 	}
 }
+
+class AddSingleFileModalForm extends Component {
+
+	render() {
+		const {getFieldDecorator} = this.props.form
+
+		const formItemLayout = {
+			labelCol: {span: 5},
+			wrapperCol: {span: 19}
+		}
+
+		return (
+			<Form layout='horizontal' onSubmit={this.handleSubmit}>
+				<Form.Item label='全宗号' {...formItemLayout}>
+					{
+						getFieldDecorator('fondsCode', {
+						})(
+							<Input
+								prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+								placeholder="全宗号"
+							/>,
+						)
+					}
+				</Form.Item>
+
+				<Form.Item label='文件标题' {...formItemLayout}>
+					{
+						getFieldDecorator('title', {
+						})(
+							<Input
+								prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+								placeholder="文件标题"
+							/>,
+						)
+					}
+				</Form.Item>
+			</Form>
+		)
+	}
+
+}
+
+AddSingleFileModalForm = Form.create({})(AddSingleFileModalForm)
 
 export default SingleFile
