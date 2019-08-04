1e95a4c119d052360999f813f87840e6ab8e7210
HytonightYX <whosiyuan@qq.com>
2019-08-04 17:48:21 +0800

    feat: 获取时间/日期字符串工具函数

--- src/services/utils.js
+++ src/services/utils.js
@@ -5,3 +5,31 @@
 export function loadConfigAsGlobal(config) {
 	global.config = config
 }
+
+/**
+ * 获取时间/日期字符串
+ * @param onlyDate
+ * @returns {string}
+ */
+export function getTime(onlyDate = false, isNow = true) {
+	let now = new Date()
+	let year = now.getFullYear()
+	let month = now.getMonth() + 1
+	let day = now.getDate()
+
+	// yyyy-MM-dd
+	if (onlyDate) {
+
+		const date = (
+			(year < 10 ? '0' + year : year)
+			+ '-' +
+			(month < 10 ? '0' + month : month)
+			+ '-' +
+			(day < 10 ? '0' + day : day)
+		)
+
+		return date
+	} else {
+		return now.toString()
+	}
+}
