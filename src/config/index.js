9b13e95f51b4041208ab7d14c79bb972de15a3a5
HytonightYX <whosiyuan@qq.com>
2019-08-19 18:08:12 +0800

    fix: 导出config出错

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,8 @@
-const env = 'dev'
-const mockURL = 'https://www.easy-mock.com/mock/5d341175763f0068c2361e65/api'
-const url = 'http://localhost:3035'
 
-export default {
-	env,
-	mockURL,
-	url
+const config = {
+	env: 'dev',
+	mockURL: 'https://www.easy-mock.com/mock/5d341175763f0068c2361e65/api',
+	url: 'http://localhost:3035'
 }
+
+export default config
