5c9df0d2b315f6235236876ba546cab92b6d1fed
HytonightYX <whosiyuan@qq.com>
2019-09-09 14:06:35 +0800

    feat: 退出登录

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,51 +1,59 @@
 import React from 'react'
 import { Icon, Layout, Avatar, Dropdown, Menu } from 'antd'
+import { Link } from 'react-router-dom'
 import './style.less'
+import { inject, observer } from 'mobx-react'
 
 const {Header} = Layout
 
-const FmsHeader = (props) => (
-	<Header className='header'>
-		<span className='header-title'>
-			文件档案管理
-		</span>
+@inject('userStore')
+@observer
+class FmsHeader extends React.Component {
+	UserMenu = (
+		<Menu>
+			<Menu.Item>
+				<Link to='/user'>
+					个人中心
+				</Link>
+			</Menu.Item>
+			<Menu.Item>
+				<a onClick={this.props.togglePwdModal}>
+					修改密码
+				</a>
+			</Menu.Item>
+			<Menu.Item>
+				<a onClick={() => this.props.userStore.logout()}>
+					退出登录
+				</a>
+			</Menu.Item>
+		</Menu>
+	)
 
-		<span className='user'>
-			您好,{props.currentUser
-			? () => (
-				props.currentUser.username +
-				<Dropdown overlay={UserMenu} className='dropdown'>
-				<span>
-					<Avatar shape="square" icon="user"/>
-					&nbsp;&nbsp;
-					<Icon type="caret-down" />
+	render() {
+		return (
+			<Header className='header'>
+				<span className='header-title'>
+					文件档案管理
 				</span>
-				</Dropdown>
-			)
-			: '请先登录'
-		} &nbsp;&nbsp;
-		</span>
-	</Header>
-)
 
-const UserMenu = (
-	<Menu>
-		<Menu.Item>
-			<a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-				1st menu item
-			</a>
-		</Menu.Item>
-		<Menu.Item>
-			<a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
-				2nd menu item
-			</a>
-		</Menu.Item>
-		<Menu.Item>
-			<a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-				3rd menu item
-			</a>
-		</Menu.Item>
-	</Menu>
-)
+				<span className='user'>
+				{this.props.currentUser
+					? <span>
+							{this.props.currentUser.username}&nbsp;&nbsp;
+							<Dropdown overlay={this.UserMenu} className='dropdown'>
+								<span>
+									<Avatar shape="square" icon="user"/>
+									&nbsp;&nbsp;
+									<Icon type="caret-down"/>
+								</span>
+							</Dropdown>
+						</span>
+					: <Link to={'/'}>登录</Link>
+				} &nbsp;&nbsp;
+				</span>
+			</Header>
+		)
+	}
+}
 
 export default FmsHeader
